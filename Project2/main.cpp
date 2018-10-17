8145d73774d1c1b64bc8379f58df4c4581354498
Joseph Lovoi <joelovoi@gmail.com>
2018-10-17 11:10:49 -0500

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Joseph Lovoi on 10/17/18.
+//  Copyright © 2018 Joseph Lovoi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
